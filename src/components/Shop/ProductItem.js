d2c11c8105b548df62a713d2bb8b410c221f9727
Somesh <Somesh.4543@gmail.com>
2022-04-02 18:09:49 +0530

    useEffect

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,19 +1,27 @@
-import { useDispatch } from 'react-redux';
-import { cartActions } from '../../Store/cart-slice';
-import Card from '../UI/Card';
-import classes from './ProductItem.module.css';
+import { useDispatch } from "react-redux";
+import { cartActions } from "../../Store/cart-slice";
+
+import Card from "../UI/Card";
+import classes from "./ProductItem.module.css";
 
 const ProductItem = (props) => {
+  
   const dispatch = useDispatch();
 
   const { title, price, description, id } = props;
 
   const addToCartHandler = () => {
-    dispatch(cartActions.addItemToCart({
-      id,
-      title,
-      price,
-    }));
+   
+    // and then send Http request
+    // fetch('firebase-url', { method: 'POST', body: JSON.stringify(newCart) })
+
+    dispatch(
+      cartActions.addItemToCart({
+        id,
+        title,
+        price,
+      })
+    );
   };
 
   return (
